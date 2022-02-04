3f67d1cfe35184144f55f2485df0ee27d0e74b42
zlotawanna@gmail.com <zlotawanna@gmail.com>
2022-02-04 20:01:05 +0100

    database for products

---
+++ db/migrate/20220204184217_create_products.rb
@@ -0,0 +1,11 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.string :category
+      t.string :name
+      t.float :price
+
+      t.timestamps
+    end
+  end
+end
